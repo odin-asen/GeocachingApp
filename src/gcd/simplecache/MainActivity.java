db9294cdcb79b71659ac9aa9cc3dbc0fc6af060f
odin-asen <geroge.forman@freenet.de>
2013-03-23 23:48:23 +0000

    Remove hello_world string value and add tab titles to resources. Make tab creation easier to understand and get rid of magic values.

--- src/gcd/simplecache/MainActivity.java
+++ src/gcd/simplecache/MainActivity.java
@@ -1,12 +1,16 @@
 package gcd.simplecache;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
 import android.view.Menu;
+import android.widget.TabHost;
 
 public class MainActivity extends FragmentActivity {
+  private static final String ID_TS_MAP = "map";
+  private static final String ID_TS_COMPASS = "compass";
   private FragmentTabHost mTabHost;
 
   @Override
@@ -17,12 +21,9 @@ public class MainActivity extends FragmentActivity {
     mTabHost = (FragmentTabHost)findViewById(android.R.id.tabhost);
     mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
 
-    /* Add tab for the cache map */
-    mTabHost.addTab(mTabHost.newTabSpec("map").setIndicator("Map"),
-        Fragment.class, null);
-    /* Add tab for the compass */
-    mTabHost.addTab(mTabHost.newTabSpec("compass").setIndicator("Compass"),
-        Fragment.class, null);
+    /* Add map and compass tabs */
+    addTab(ID_TS_MAP, this.getString(R.string.tab_title_map), null, Fragment.class);
+    addTab(ID_TS_COMPASS, this.getString(R.string.tab_title_compass), null, Fragment.class);
   }
 
 	@Override
@@ -31,4 +32,11 @@ public class MainActivity extends FragmentActivity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+  /* Helper method that creates and adds a tab to the TabHost tabHost */
+  private void addTab(String id, String title, Drawable icon, Class fragmentClass) {
+    TabHost.TabSpec tabSpec = mTabHost.newTabSpec(id);
+    tabSpec.setIndicator(title, icon);
+    mTabHost.addTab(tabSpec, fragmentClass, null);
+  }
 }
