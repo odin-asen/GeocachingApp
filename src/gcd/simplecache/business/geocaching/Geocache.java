2cff5c6f0265a8c76b47b818391748b84bd34ee7
odin-asen <geroge.forman@freenet.de>
2013-04-14 00:14:13 +0100

    Initial commit. Create abstract class GeocachingService and class Geocache

---
+++ src/gcd/simplecache/business/geocaching/Geocache.java
@@ -0,0 +1,13 @@
+package gcd.simplecache.business.geocaching;
+
+/**
+ * This class represents a geocache.
+ * <p/>
+ * Author: Timm Herrmann<br/>
+ * Date: 14.04.13
+ */
+public class Geocache {
+  /* Constructors */
+  /* Methods */
+  /* Getter and Setter */
+}
---
+++ src/gcd/simplecache/business/geocaching/GeocachingService.java
@@ -0,0 +1,15 @@
+package gcd.simplecache.business.geocaching;
+
+/**
+ * Represents a geocaching service. This class can be extended to provide
+ * connection to different geocaching services, such as geocaching.com and
+ * opencaching.com.
+ * <p/>
+ * Author: Timm Herrmann<br/>
+ * Date: 14.04.13
+ */
+public abstract class GeocachingService {
+  /* Constructors */
+  /* Methods */
+  /* Getter and Setter */
+}
