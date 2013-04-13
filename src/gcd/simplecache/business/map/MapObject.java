b133590a0262438f0507a4ef8edd051c4ab967ad
odin-asen <geroge.forman@freenet.de>
2013-04-13 21:18:05 +0100

    ObjectType describes the type of a MapObject

--- src/gcd/simplecache/business/map/MapObject.java
+++ src/gcd/simplecache/business/map/MapObject.java
@@ -9,16 +9,34 @@ import org.osmdroid.views.overlay.OverlayItem;
  * Time: 21:12
  */
 public class MapObject extends OverlayItem {
+  private ObjectType type;
+
   /* Constructors */
   public MapObject(String aTitle, String aDescription, GeoPoint aGeoPoint) {
     super(aTitle, aDescription, aGeoPoint);
   }
 
   /* Methods */
+
   /* Getter and Setter */
+  public void setObjectType(ObjectType type) {
+    this.type = type;
+  }
+
+  public ObjectType getObjectType() {
+    return type;
+  }
 
   /* Inner classes */
   public enum ObjectType {
+    AIM,
+    MULTI,
+    RIDDLE,
+    TRADITIONAL,
+    USER;
 
+    public boolean isGeocache() {
+      return !(this.equals(AIM) || this.equals(USER));
+    }
   }
 }
