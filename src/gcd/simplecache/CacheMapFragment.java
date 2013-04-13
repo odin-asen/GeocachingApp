0171a4872f064a61a8afebb805cf414c5cf4563a
odin-asen <geroge.forman@freenet.de>
2013-04-13 21:25:54 +0100

    Initialise MapObjectFactory. Add skeleton for method that will show the caches.

--- src/gcd/simplecache/CacheMapFragment.java
+++ src/gcd/simplecache/CacheMapFragment.java
@@ -5,6 +5,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import gcd.simplecache.business.map.MapObject;
+
+import java.util.List;
 
 /**
  * Map view fragment. This map view shows the caches on a map.
@@ -14,5 +17,36 @@ public class CacheMapFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     return inflater.inflate(R.layout.fragment_cachemap, container, false);
+  }
+
+  /**
+   * <b>This is not yet functional.</b>
+   * This method takes a list of cache information and displays it on the map.
+   * @param cacheList List with caches to show on the map.
+   */
+  public void loadMapObjects(List<Object> cacheList) {
+
+  }
+}
+
+class MapObjectFactory {
+  static MapObject createAim() {
+    return null;
+  }
+
+  static MapObject createMultiCache() {
+    return null;
+  }
+
+  static MapObject createRiddleCache() {
+    return null;
+  }
+
+  static MapObject createTraditionalCache() {
+    return null;
+  }
+
+  static MapObject createUserObject() {
+    return null;
   }
 }
