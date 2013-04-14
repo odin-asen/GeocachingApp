287b11b18c666b6458f5405caf657ed7aafb16d1
odin-asen <geroge.forman@freenet.de>
2013-04-15 00:09:05 +0100

    GeocachingService provides methods to establish and close an http connection.

--- src/gcd/simplecache/business/geocaching/GeocachingService.java
+++ src/gcd/simplecache/business/geocaching/GeocachingService.java
@@ -1,24 +1,65 @@
 package gcd.simplecache.business.geocaching;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.List;
 
 /**
  * Represents a geocaching service. This class can be extended to provide
- * connection to different geocaching services, such as geocaching.com and
- * opencaching.com.
+ * connection to different geocaching services, such as
+ * <a href=http://www.geocaching.com>geocaching.com</a> and
+ * <a href=http://www.opencaching.com>opencaching.com</a>.
  * <p/>
  * Author: Timm Herrmann<br/>
  * Date: 14.04.13
  */
 abstract public class GeocachingService {
   protected String error;
+  protected HttpURLConnection httpConn;
 
   /* Constructors */
   protected GeocachingService() {
     error = "";
+    httpConn = null;
   }
 
   /* Methods */
+
+  /**
+   * Opens an http connection to a specified url
+   * and stores it to {@link #httpConn}.<br/>
+   * The method first creates an {@link HttpURLConnection} object,
+   * sets values to follow redirects and the request method to 'GET'.
+   * The a connection will be established.
+   * @param urlString Valid http url.
+   * @throws IOException {@code urlString} is no valid http url.
+   */
+  protected void openHttpConnection(String urlString) throws IOException {
+    URL url = new URL(urlString);
+    URLConnection conn = url.openConnection();
+
+    if (!(conn instanceof HttpURLConnection))
+      throw new IOException("Not an HTTP connection");
+
+    /* setup http connection */
+    httpConn = (HttpURLConnection) conn;
+    httpConn.setInstanceFollowRedirects(true);
+    httpConn.setRequestMethod("GET");
+    httpConn.connect();
+  }
+
+  /**
+   * Disconnects the http connection. Does nothing if no connection is
+   * established. {@link #httpConn} is null after this method was called.
+   */
+  protected void closeHttpConnection() {
+    if(httpConn != null) {
+      httpConn.disconnect();
+      httpConn = null;
+    }
+  }
 
   /**
    * Logs in to the geocaching service.
