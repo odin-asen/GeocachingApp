a466a152f727182693c7ad7d304f170dc4ac2c52
odin-asen <geroge.forman@freenet.de>
2013-04-16 18:45:27 +0100

    Set up the Limit Parameter.

--- src/gcd/simplecache/business/geocaching/request/com/opencaching/Limit.java
+++ src/gcd/simplecache/business/geocaching/request/com/opencaching/Limit.java
@@ -1,23 +1,31 @@
 package gcd.simplecache.business.geocaching.request.com.opencaching;
 
+import gcd.simplecache.business.geocaching.request.ComOpencachingParameter;
+
 /**
- * limit=limit
-
- Sets the maximum number of geocaches that will be returned. Can be between 1 and 5000. Defaults to 100 if no limit is specified.
- hard_limit=true/false
-
- hard_limit = true:
- If more geocaches fit the criteria than what can be returned before reaching the the limit, then an error (HTTP error 413) is returned instead of any geocaches.
-
- hard_limit = false or not specified:
- If more geocaches fit the criteria than what can be returned before reaching the limit, then the closest geocaches to the center of the query are returned.
-
+ * Sets the maximum number of geocaches that will be returned.
+ * Defaults to 100 if no limit is specified.
  * <p/>
  * Author: Timm Herrmann<br/>
  * Date: 16.04.13
  */
-public class Limit {
+public class Limit extends ComOpencachingParameter {
+  private static final String NAME = "limit";
+  private static final String EQUALS = "=";
+
   /* Constructors */
+  /**
+   * Creates the limit parameter. Can be between 1 and 5000.
+   * @param limit Specifies the limit of caches.
+   */
+  public Limit(int limit) {
+    super(NAME, Integer.toString(limit));
+  }
+
   /* Methods */
-  /* Getter and Setter */
+
+  @Override
+  public String formatString() {
+    return name+ EQUALS + value;
+  }
 }
