7898f42c859f4cd8172f8dcb1d25851d34f4a202
odin-asen <geroge.forman@freenet.de>
2013-04-16 21:19:56 +0100

    Intialise dto for a geocache.

---
+++ src/gcd/simplecache/dto/CacheType.java
@@ -0,0 +1,14 @@
+package gcd.simplecache.dto;
+
+/**
+ * This enumeration represents a geocache type.
+ * <p/>
+ * Author: Timm Herrmann<br/>
+ * Date: 16.04.13
+ */
+public enum CacheType {
+  MULTI,
+  RIDDLE,
+  TRADITIONAL,
+  VIRTUAL;
+}
