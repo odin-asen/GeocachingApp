4a16a0481a332257e6b7cab8398de9062b2f9053
odin-asen <geroge.forman@freenet.de>
2013-04-16 19:04:06 +0100

    Set up the Name Parameter.

--- src/gcd/simplecache/business/geocaching/request/com/opencaching/Name.java
+++ src/gcd/simplecache/business/geocaching/request/com/opencaching/Name.java
@@ -1,16 +1,31 @@
 package gcd.simplecache.business.geocaching.request.com.opencaching;
 
+import gcd.simplecache.business.geocaching.request.ComOpencachingParameter;
+
 /**
- * name=name
-
- Limits returned geocaches to those containing the string somewhere in their title. The search string does not have to start at the beginning of a word in the title.
-
+ * Limits returned geocaches to those containing a string somewhere in their title.
+ * The search string does not have to start at the beginning of a word in the title.
  * <p/>
  * Author: Timm Herrmann<br/>
  * Date: 16.04.13
  */
-public class Name {
+public class Name extends ComOpencachingParameter {
+  private static final String NAME = "name";
+  private static final String EQUALS = "=";
+
   /* Constructors */
+  /**
+   * Creates the name parameter.
+   * @param value String that is in the title of a cache.
+   */
+  public Name(String value) {
+    super(NAME, value);
+  }
+
   /* Methods */
-  /* Getter and Setter */
+
+  @Override
+  public String formatString() {
+    return name+ EQUALS + value;
+  }
 }
