cdd4e410e009edc0cac27711e2ab8244cbc942af
odin-asen <geroge.forman@freenet.de>
2013-04-19 23:36:35 +0100

    Call method to change user position on the map

--- src/gcd/simplecache/MainActivity.java
+++ src/gcd/simplecache/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentTabHost;
@@ -13,8 +14,8 @@ import android.view.Menu;
 import android.widget.TabHost;
 
 public class MainActivity extends FragmentActivity {
-  private static final String ID_TS_MAP = "map";
-  private static final String ID_TS_COMPASS = "compass";
+  private static final String TAG_TS_MAP = "map";
+  private static final String TAG_TS_COMPASS = "compass";
   
   private static final String ACTION_ID_GPS = "LocationChanged";
   private static final String ACTION_ID_COMPASS = "SensorChanged";  
@@ -34,8 +35,8 @@ public class MainActivity extends FragmentActivity {
     mTabHost.setup(this, getSupportFragmentManager(), R.id.realtabcontent);
 
     /* Add map and compass tabs */
-    addTab(ID_TS_MAP, this.getString(R.string.tab_title_map), null, CacheMapFragment.class);
-    addTab(ID_TS_COMPASS, this.getString(R.string.tab_title_compass), null, CompassFragment.class);
+    addTab(TAG_TS_MAP, this.getString(R.string.tab_title_map), null, CacheMapFragment.class);
+    addTab(TAG_TS_COMPASS, this.getString(R.string.tab_title_compass), null, CompassFragment.class);
 
     receiver = new MessageReceiver();
     receiverRegistered = false;
@@ -78,20 +79,32 @@ public class MainActivity extends FragmentActivity {
 
 		@Override
 		public void onReceive(Context context, Intent intent) {
-			if (intent.getAction() == ACTION_ID_GPS){
+			if (intent.getAction().equals(ACTION_ID_GPS)){
 				Bundle extras = intent.getExtras();
 				Double lat = extras.getDouble("lat");
 				Double lon = extras.getDouble("lon");
-				if (mTabHost.getCurrentTabTag() == ID_TS_COMPASS) {
-					CompassFragment compass = (CompassFragment) getSupportFragmentManager().findFragmentByTag(ID_TS_COMPASS);
-					compass.update(lat, lon);
-				}
-				Log.d("Loc","Changed");
-			} if (intent.getAction() == ACTION_ID_COMPASS) {	
+        changeLocation(lat, lon);
+        Log.d("Loc","Changed");
+			} if (intent.getAction().equals(ACTION_ID_COMPASS)) {
 				Log.d("Sensor", "Changed");
 			}
 		}
-	}
+
+    private void changeLocation(Double lat, Double lon) {
+      final String currentTabTag = mTabHost.getCurrentTabTag();
+
+      if (currentTabTag.equals(TAG_TS_COMPASS)) {
+        CompassFragment compass = (CompassFragment) getSupportFragmentManager().findFragmentByTag(TAG_TS_COMPASS);
+        compass.update(lat, lon);
+      } else if(currentTabTag.equals(TAG_TS_MAP)) {
+        CacheMapFragment map = (CacheMapFragment) getSupportFragmentManager().findFragmentByTag(TAG_TS_MAP);
+        Location location = new Location(getString(R.string.app_name));
+        location.setLatitude(lat);
+        location.setLongitude(lon);
+        map.updateUserPosition(location);
+      }
+    }
+  }
 }
 
 
