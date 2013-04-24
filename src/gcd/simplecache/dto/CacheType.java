5fc7d434c09f17508a76334f572e7cfbb6152822
odin-asen <geroge.forman@freenet.de>
2013-04-24 15:04:18 +0100

    Extend CacheType with a default object. It can parse a cache type by a string.

--- src/gcd/simplecache/dto/CacheType.java
+++ src/gcd/simplecache/dto/CacheType.java
@@ -7,8 +7,37 @@ package gcd.simplecache.dto;
  * Date: 16.04.13
  */
 public enum CacheType {
-  MULTI,
-  RIDDLE,
-  TRADITIONAL,
-  VIRTUAL;
+  DEFAULT(""),
+  MULTI("multi"),
+  RIDDLE("unknown"),
+  TRADITIONAL("traditional"),
+  VIRTUAL("virtual");
+
+  private String description;
+
+  CacheType(String description) {
+    this.description = description;
+  }
+
+  /**
+   * Parse a string and return the enumeration object
+   * Which is contained in that string. The parsing
+   * is case insensitive. If no enumeration matches,
+   * the default object will be returned.
+   */
+  CacheType parseCacheType(String description) {
+    if(description == null)
+      return DEFAULT;
+
+    final CacheType[] values = CacheType.values();
+
+    description = description.toLowerCase();
+    for (int index = 1; index < values.length; index++) {
+      final CacheType type = values[index];
+      if(description.contains(type.description))
+        return type;
+    }
+
+    return DEFAULT;
+  }
 }
