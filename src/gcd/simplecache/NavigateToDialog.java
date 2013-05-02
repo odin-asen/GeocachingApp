ef642710d0d4687c30d980d8a6bc45288196ee6c
odin-asen <geroge.forman@freenet.de>
2013-05-02 15:18:40 +0100

    NavigationTo dialog checks for valid numbers for the coordinate fields.

--- src/gcd/simplecache/NavigateToDialog.java
+++ src/gcd/simplecache/NavigateToDialog.java
@@ -1,9 +1,5 @@
 package gcd.simplecache;
 
-import android.text.TextUtils;
-import android.widget.Toast;
-import gcd.simplecache.business.map.GeoCoordinateConverter;
-import gcd.simplecache.dto.geocache.DTOGeocache;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
@@ -15,13 +11,24 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.Toast;
+import gcd.simplecache.business.map.GeoCoordinateConverter;
+import gcd.simplecache.dto.geocache.DTOGeocache;
 
 public class NavigateToDialog extends DialogFragment implements IntentActions, OnCheckedChangeListener {
 	
 	private View navView; 
 	private RadioGroup navGroup;
 	private boolean isDegree;
-	
+
+  /* Editable Views */
+  private EditText mLatDecDeg;
+  private EditText mLonDecDeg;
+  private EditText mLatDecMinDeg;
+  private EditText mLatDecMinMin;
+  private EditText mLonDecMinDeg;
+  private EditText mLonDecMinMin;
+
 	public NavigateToDialog()  {
 
 	}
@@ -30,40 +37,52 @@ public class NavigateToDialog extends DialogFragment implements IntentActions, O
   @Override
   public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-   AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-   LayoutInflater inflater = getActivity().getLayoutInflater();
-   navView = inflater.inflate(R.layout.dialog_navigate_to, null);
+    LayoutInflater inflater = getActivity().getLayoutInflater();
+    navView = inflater.inflate(R.layout.dialog_navigate_to, null);
+    initialiseEditVariables();
 
-   builder.setView(navView);
+    builder.setView(navView);
 
-   navGroup = (RadioGroup)navView.findViewById(R.id.navGroup);
-   navGroup.setOnCheckedChangeListener(this);
-   setDegreeEnabled();
+    navGroup = (RadioGroup)navView.findViewById(R.id.navGroup);
+    navGroup.setOnCheckedChangeListener(this);
+    setDegreeEnabled();
 
-   builder.setPositiveButton(R.string.button_ok, new OkListener());
-   builder.setNegativeButton(R.string.button_cancel, new CancelListener());
+    builder.setPositiveButton(R.string.button_ok, new OkListener());
+    builder.setNegativeButton(R.string.button_cancel, new CancelListener());
 
-   return builder.create();
- }
+    return builder.create();
+  }
+
+  private void initialiseEditVariables() {
+    mLatDecDeg = (EditText) navView.findViewById(R.id.text_lat_dec_deg);
+    mLonDecDeg = (EditText) navView.findViewById(R.id.text_lon_dec_deg);
+    mLatDecMinDeg = (EditText) navView.findViewById(R.id.text_lat_dec_min_deg);
+    mLatDecMinMin = (EditText) navView.findViewById(R.id.text_lat_dec_min_min);
+    mLonDecMinDeg = (EditText) navView.findViewById(R.id.text_lon_dec_min_deg);
+    mLonDecMinMin = (EditText) navView.findViewById(R.id.text_lon_dec_min_min);
+  }
 
 	private class OkListener implements DialogInterface.OnClickListener {
 
     @Override
 		public void onClick(DialogInterface dialog, int which) {
       AlertDialog alert = (AlertDialog) dialog;
-      if(inputIsEmpty()) {
+
+      if(inputIsValid()) {
         Intent navigationAction = new Intent(ACTION_ID_NAVIGATION);
         navigationAction.putExtra(NAVIGATION_ENABLED, true);
         navigationAction.putExtra(NAVIGATION_DESTINATION, createDTO());
         getActivity().sendBroadcast(navigationAction);
       } else {
-        Toast.makeText(alert.getContext(), "Please fill out all required fields!", Toast.LENGTH_LONG);
-
+        Toast.makeText(alert.getContext(),
+            "Navigation did not change! Please fill out all fields with valid values!",
+            Toast.LENGTH_LONG).show();
       }
 		}
 	}
-	
+
 	private class CancelListener implements DialogInterface.OnClickListener {
 		@Override
 		public void onClick(DialogInterface dialog, int which) {
@@ -82,58 +101,40 @@ public class NavigateToDialog extends DialogFragment implements IntentActions, O
 				break;
 		}
 	}
-	
+
 	private void setDegreeEnabled() {
-		EditText editText = (EditText)navView.findViewById(R.id.text_lon_dec_deg);
-		editText.setEnabled(true);
-		editText = (EditText)navView.findViewById(R.id.text_lat_dec_deg);
-		editText.setEnabled(true);
-		editText = (EditText)navView.findViewById(R.id.text_lon_dec_min_deg);
-		editText.setEnabled(false);
-		editText = (EditText)navView.findViewById(R.id.text_lon_dec_min_min);
-		editText.setEnabled(false);
-		editText = (EditText)navView.findViewById(R.id.text_lat_dec_min_deg);
-		editText.setEnabled(false);
-		editText = (EditText)navView.findViewById(R.id.text_lat_dec_min_min);
-		editText.setEnabled(false);
+		mLonDecDeg.setEnabled(true);
+		mLatDecDeg.setEnabled(true);
+		mLonDecMinDeg.setEnabled(false);
+		mLonDecMinMin.setEnabled(false);
+		mLatDecMinDeg.setEnabled(false);
+		mLatDecMinMin.setEnabled(false);
 		isDegree = true;
 	}
 	
 	private void setMinuteEnabled() {
-		EditText editText = (EditText)navView.findViewById(R.id.text_lon_dec_deg);
-		editText.setEnabled(false);
-		editText = (EditText)navView.findViewById(R.id.text_lat_dec_deg);
-		editText.setEnabled(false);
-		editText = (EditText)navView.findViewById(R.id.text_lon_dec_min_deg);
-		editText.setEnabled(true);
-		editText = (EditText)navView.findViewById(R.id.text_lon_dec_min_min);
-		editText.setEnabled(true);
-		editText = (EditText)navView.findViewById(R.id.text_lat_dec_min_deg);
-		editText.setEnabled(true);
-		editText = (EditText)navView.findViewById(R.id.text_lat_dec_min_min);
-		editText.setEnabled(true);
+    mLonDecDeg.setEnabled(false);
+    mLatDecDeg.setEnabled(false);
+    mLonDecMinDeg.setEnabled(true);
+    mLonDecMinMin.setEnabled(true);
+    mLatDecMinDeg.setEnabled(true);
+    mLatDecMinMin.setEnabled(true);
 		isDegree = false;
 	}
 	
 	private DTOGeocache createDTO () {
 		DTOGeocache dto = new DTOGeocache();
 		if (isDegree) {
-			EditText editText = (EditText)navView.findViewById(R.id.text_lon_dec_deg);
-			dto.location.longitude = Double.parseDouble(editText.getText().toString());
-			editText = (EditText)navView.findViewById(R.id.text_lat_dec_deg);
-			dto.location.latitude = Double.parseDouble(editText.getText().toString());
+			dto.location.longitude = Double.parseDouble(mLonDecDeg.getText().toString());
+			dto.location.latitude = Double.parseDouble(mLatDecDeg.getText().toString());
 		}
 		else {
 			GeoCoordinateConverter converter = new GeoCoordinateConverter();
-			EditText editText = (EditText)navView.findViewById(R.id.text_lon_dec_deg);
-			int degrees = Integer.parseInt(editText.getText().toString());
-			editText = (EditText)navView.findViewById(R.id.text_lon_dec_min_min);
-			double minutes = Double.parseDouble(editText.getText().toString());
+			int degrees = Integer.parseInt(mLonDecMinDeg.getText().toString());
+			double minutes = Double.parseDouble(mLonDecMinMin.getText().toString());
 			double longitude = converter.decimalMinutesToDecimalDegrees(degrees, minutes);
-			editText = (EditText)navView.findViewById(R.id.text_lat_dec_min_deg);
-			degrees = Integer.parseInt(editText.getText().toString());
-			editText = (EditText)navView.findViewById(R.id.text_lat_dec_min_min);
-			minutes = Double.parseDouble(editText.getText().toString());
+			degrees = Integer.parseInt(mLatDecMinDeg.getText().toString());
+			minutes = Double.parseDouble(mLatDecMinMin.getText().toString());
 			double latitude = converter.decimalMinutesToDecimalDegrees(degrees, minutes);
 			dto.location.longitude = longitude;
 			dto.location.latitude = latitude;
@@ -141,28 +142,30 @@ public class NavigateToDialog extends DialogFragment implements IntentActions, O
 		return dto;
 	}
 
-  /** Check if the text fields are empty or not. */
-  private boolean inputIsEmpty() {
-    EditText editText;
+  /** Check if the text fields have correct values. */
+  private boolean inputIsValid() {
     boolean result;
 
     if(isDegree) {
-      editText = (EditText)navView.findViewById(R.id.text_lon_dec_deg);
-      result = TextUtils.isEmpty(editText.getText().toString());
-      editText = (EditText)navView.findViewById(R.id.text_lat_dec_deg);
-      result = result || TextUtils.isEmpty(editText.getText().toString());
+      result = checkEditText(mLonDecDeg, -180.0, 180.0);
+      result = result && checkEditText(mLatDecDeg, -90.0, 90.0);
     } else {
-      editText = (EditText)navView.findViewById(R.id.text_lon_dec_deg);
-      result = TextUtils.isEmpty(editText.getText().toString());
-      editText = (EditText)navView.findViewById(R.id.text_lon_dec_min_min);
-      result = result || TextUtils.isEmpty(editText.getText().toString());
-      editText = (EditText)navView.findViewById(R.id.text_lat_dec_min_deg);
-      result = result || TextUtils.isEmpty(editText.getText().toString());
-      editText = (EditText)navView.findViewById(R.id.text_lat_dec_min_min);
-      result = result || TextUtils.isEmpty(editText.getText().toString());
+      result = checkEditText(mLonDecMinDeg, -180.0, 180.0);
+      result = result && checkEditText(mLonDecMinMin, 0, 60);
+      result = result && checkEditText(mLatDecMinDeg, -90, 90);
+      result = result && checkEditText(mLatDecMinMin, 0, 60);
     }
 
     return result;
+  }
+
+  private boolean checkEditText(EditText editText, double min, double max) {
+    try {
+      double value = Double.parseDouble(editText.getText().toString());
+      return (value >= min) && (value <= max);
+    } catch (NumberFormatException e) {
+      return false;
+    }
   }
 }
 
